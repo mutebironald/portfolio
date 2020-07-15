ec300006de9dd75204975890ac0e897d8abbb912
Ronald Mutebi <mutebironaldroninho@gmail.com>
2020-07-15 12:24:47 +0300

    Updates

---
+++ precache-manifest.8a5356fb6277a14b76522d971112ab72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "926406a69f96bf06cbbe6adf6b2cd5e3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9ccbaec74dd60482c818",
+    "url": "/portfolio/static/css/main.138a6cb0.chunk.css"
+  },
+  {
+    "revision": "81cd87485ae32ef7ae2e",
+    "url": "/portfolio/static/js/2.0c579f19.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/portfolio/static/js/2.0c579f19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ccbaec74dd60482c818",
+    "url": "/portfolio/static/js/main.c1491e22.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
